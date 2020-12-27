5f90c2ade36de6122f783ede664388c0b7868963
Rehlat Online Services Pvt Ltd <joy.vincent@rehlat.com>
2020-12-27 14:05:59 +0530

    Initial Commit

---
+++ CardStack/CardStack.h
@@ -0,0 +1,18 @@
+//
+//  CardStack.h
+//  CardStack
+//
+//  Created by Rehlat Online Services Pvt Ltd on 27/12/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CardStack.
+FOUNDATION_EXPORT double CardStackVersionNumber;
+
+//! Project version string for CardStack.
+FOUNDATION_EXPORT const unsigned char CardStackVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CardStack/PublicHeader.h>
+
+
